141b7aa03ca35df96bc2f6d0ddc1fb89f7e55697
Marek <marek.j.swakon@gmail.com>
2024-12-09 15:53:16 +0100

    zabawa z textField

--- oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
+++ oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
@@ -4,15 +4,23 @@ import agh.ics.oop.model.MapChangeListener;
 import agh.ics.oop.model.WorldMap;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class SimulationPresenter implements MapChangeListener {
     public WorldMap map;
+    public Label moveDescription;
 
     @FXML
     private Label infoLabel;
+    @FXML
+    private TextField arguments;
 
     public void setWorldMap(WorldMap map) {
         this.map = map;
+    }
+
+    public String getArguments(){
+        return arguments.getText();
     }
 
     private void drawMap(){
@@ -23,4 +31,6 @@ public class SimulationPresenter implements MapChangeListener {
     public void mapChanged(WorldMap map, String message) {
         drawMap();
     }
+
+
 }
