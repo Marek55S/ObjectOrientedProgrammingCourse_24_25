c45592f503f147d9c2e19fba7da1c4c4a24bf08e
Marek <marek.j.swakon@gmail.com>
2024-12-10 20:49:26 +0100

    mapGrid
    do poprawy

--- oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
+++ oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
@@ -9,6 +9,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 
 import java.util.List;
 
@@ -16,6 +19,9 @@ public class SimulationPresenter implements MapChangeListener {
     public WorldMap map;
     public Label moveDescription;
     public Button startButton;
+    public GridPane mapGrid;
+    public static double CELL_WIDTH = 20;
+    public static double CELL_HEIGHT = 30;
 
     @FXML
     private Label infoLabel;
@@ -30,9 +36,37 @@ public class SimulationPresenter implements MapChangeListener {
         return arguments.getText().split(" ");
     }
 
-    private void drawMap(){
-        infoLabel.setText(map.toString());
+    private void clearGrid() {
+        mapGrid.getChildren().retainAll(mapGrid.getChildren().getFirst()); // hack to retain visible grid lines
+        mapGrid.getColumnConstraints().clear();
+        mapGrid.getRowConstraints().clear();
     }
+
+    private void drawIndexes(){
+        int minWidth = map.getCurrentBounds().LowerLeft().getX()-1;
+        int minHeight = map.getCurrentBounds().LowerLeft().getY()-1;
+        int mapWidth = map.getCurrentBounds().UpperRight().getX()-minWidth;
+        int mapHeight = map.getCurrentBounds().UpperRight().getY()-minHeight;
+        for(int i=minWidth+1;i<mapWidth;i++) mapGrid.add(new Label(String.valueOf(i)),i,minWidth);
+        for(int i=minHeight+1;i<mapHeight;i++) mapGrid.add(new Label(String.valueOf(i)),minHeight,i);
+        mapGrid.add(new Label("y/x"),minWidth,minHeight);
+
+
+    }
+
+
+    private void drawElements(){
+        for(WorldElement element : map.getElements()){
+            mapGrid.add(new Label(element.toString()),element.getPosition().getX(),element.getPosition().getY());
+        }
+    }
+
+    private void drawMap(){
+        clearGrid();
+        mapGrid.getColumnConstraints().add(new ColumnConstraints(CELL_WIDTH));
+        mapGrid.getRowConstraints().add(new RowConstraints(CELL_HEIGHT));
+    }
+
 
     @Override
     public void mapChanged(WorldMap map, String message) {
